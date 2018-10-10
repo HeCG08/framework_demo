891a50fd2fdc2f99385bd20a2a9dfc943f6e6c0f
Changgen He <he.changgen@qq.com>
2018-10-10 09:19:10 +0800

    Redis module - Resolve use redisTemplate unrecognizable characters.

--- redis_demo/src/test/java/cn/rumoss/fw/redis/RedisTest.java
+++ redis_demo/src/test/java/cn/rumoss/fw/redis/RedisTest.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -12,18 +15,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class RedisTest {
 
     @Autowired
+    //private StringRedisTemplate redisTemplate;// (1) 解决存储键值乱码
     private RedisTemplate redisTemplate;
+
+    /**
+     * 解决Redis乱码问题
+     * @param redisTemplate
+     */
+    @Autowired(required = false)
+    public void setRedisTemplate(RedisTemplate redisTemplate) {
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setValueSerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
+        this.redisTemplate = redisTemplate;
+    }
 
     @Test
     public void testInsert(){
         String name = "Tanijia";
-        redisTemplate.opsForValue().set("name",name);
+        redisTemplate.opsForValue().set("name_2",name);
     }
 
     @Test
     public void testQuery(){
         String name = null;
-        name = (String) redisTemplate.opsForValue().get("name");
+        name = (String) redisTemplate.opsForValue().get("name_2");
         System.out.println("name from redis: " + name);
     }
 
